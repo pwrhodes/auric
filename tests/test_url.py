b67d2cef22b2d615c5f02a3626f5ff85cfa10f49
Peter Rhodes <peter.rhodes@devicie.com>
2023-11-04 10:52:45 +0000

    Moved test params to namedtuple

--- tests/test_url.py
+++ tests/test_url.py
@@ -1,6 +1,5 @@
 """Test the URL class."""
-from dataclasses import dataclass
-from typing import Any
+from collections import namedtuple
 
 import pytest
 
@@ -8,30 +7,29 @@ from auric import URL
 
 TEST_HOSTNAME = "auric.test"
 
-@dataclass
-class test_input():
-    """Inputs for tests."""
-    input_value: Any
-    expected_result: Any
+TestInput = namedtuple("TestInput", ["input_value", "expected_result"])
+
 
 def test_url():
     """Test basic initialisation of url and presence of attribute."""
     my_url = URL(hostname=TEST_HOSTNAME)
     assert hasattr(my_url, "as_string")
 
+
 def test_required_args__url():
     """Test basic initialisation of url without required argument."""
     with pytest.raises(TypeError):
-        URL()
+        URL()  # pylint: disable=no-value-for-parameter
+
 
 @pytest.mark.parametrize(
-        "protocol", 
-        [
-            test_input("https", True),
-            test_input("http", True),
-            test_input("test", False)
-        ]
-    )
+    "protocol",
+    [
+        TestInput(input_value="https", expected_result=True),
+        TestInput(input_value="http", expected_result=True),
+        TestInput(input_value="test", expected_result=False),
+    ],
+)
 def test_protocols__url(protocol):
     """Test initialisation of url various protocols."""
     if protocol.expected_result:
@@ -40,17 +38,35 @@ def test_protocols__url(protocol):
         with pytest.raises(ValueError):
             URL(hostname=TEST_HOSTNAME, protocol=protocol.input_value)
 
+
+def test_str__url():
+    """Test as_string result type."""
+    my_url = URL(hostname=TEST_HOSTNAME)
+    result = str(my_url)
+    assert isinstance(result, str)
+
+
 def test_as_string__url():
     """Test as_string result type."""
     my_url = URL(hostname=TEST_HOSTNAME)
     result = my_url.as_string()
     assert isinstance(result, str)
 
+
+def test_string_consistency__url():
+    """Test as_string result type."""
+    my_url = URL(hostname=TEST_HOSTNAME)
+    as_string_result = my_url.as_string()
+    str_result = str(my_url)
+    assert as_string_result == str_result
+
+
 def test_as_string_content__url():
     """Test contents of url as_string with no optional arguments."""
     my_url = URL(hostname=TEST_HOSTNAME)
     result = my_url.as_string()
-    assert result == TEST_HOSTNAME 
+    assert result == TEST_HOSTNAME
+
 
 def test_as_string_content_protocol__url():
     """Test contents of url as_string with protocol argument."""
@@ -59,12 +75,14 @@ def test_as_string_content_protocol__url():
     result = my_url.as_string()
     assert protocol in result
 
+
 def test_as_string_content_port__url():
     """Test contents of url as_string with protocol argument."""
     port = 443
     my_url = URL(hostname=TEST_HOSTNAME, port=port)
     result = my_url.as_string()
     assert str(port) in result
+
 
 def test_as_string_content_path__url():
     """Test contents of url as_string with protocol argument."""
@@ -73,11 +91,13 @@ def test_as_string_content_path__url():
     result = my_url.as_string()
     assert path in result
 
+
 def test_as_string_content_query__url():
     """Test contents of url as_string with protocol argument."""
     query = "test"
     my_url = URL(hostname=TEST_HOSTNAME, query=query)
     result = my_url.as_string()
     assert query in result
+
 
 # TODO: Test the order of URL attributes in string
